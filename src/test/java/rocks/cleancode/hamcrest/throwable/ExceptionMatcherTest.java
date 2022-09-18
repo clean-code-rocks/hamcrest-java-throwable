95a8e9677d25e9e394057b8c8ee20069c7333fbf
Benjamin PREVOT <me@benjaminprevot.fr>
2022-09-18 08:51:49 +0200

    refactor: Externalize runnable creation

--- src/test/java/rocks/cleancode/hamcrest/throwable/ExceptionMatcherTest.java
+++ src/test/java/rocks/cleancode/hamcrest/throwable/ExceptionMatcherTest.java
@@ -2,7 +2,8 @@ package rocks.cleancode.hamcrest.throwable;
 
 import org.junit.jupiter.api.Test;
 
-import static java.util.Objects.requireNonNull;
+import java.util.Objects;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -13,14 +14,14 @@ public class ExceptionMatcherTest {
 
     @Test
     public void should_match_exception_type() {
-        assertThat(() -> requireNonNull(null), willThrow(NullPointerException.class));
+        assertThat(requireNonNull(null), willThrow(NullPointerException.class));
     }
 
     @Test
     public void should_fail_when_exception_type_does_not_match() {
         AssertionError assertionError = assertThrows(
                 AssertionError.class,
-                () -> assertThat(() -> requireNonNull(null), willThrow(IllegalArgumentException.class))
+                () -> assertThat(requireNonNull(null), willThrow(IllegalArgumentException.class))
         );
 
         String expectedMessage = String.format(
@@ -36,7 +37,7 @@ public class ExceptionMatcherTest {
     public void should_fail_when_no_exception_is_thrown() {
         AssertionError assertionError = assertThrows(
                 AssertionError.class,
-                () -> assertThat(() -> requireNonNull("Dummy value"), willThrow(NullPointerException.class))
+                () -> assertThat(requireNonNull("Dummy value"), willThrow(NullPointerException.class))
         );
 
         String expectedMessage = String.format(
@@ -46,6 +47,10 @@ public class ExceptionMatcherTest {
         );
 
         assertThat(assertionError.getMessage(), is(equalTo(expectedMessage)));
+    }
+
+    private Runnable requireNonNull(Object value) {
+        return () -> Objects.requireNonNull(value);
     }
 
 }
